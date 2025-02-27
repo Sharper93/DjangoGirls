92c8772e9d5afb922c913eb060239b3e766c981f
Sammi Harper <scharper93@gmail.com>
2025-02-26 19:58:00 -0500

    Extend Application and Django forms

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), #url that thas post_list needs to execute a views function called post list
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), # url that has post_detail needs to execute a views function called post
+    path('post/new/', views.post_new, name='post_new'), # adding new path for post_new
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,11 @@
 from datetime import datetime, timezone, timedelta 
 
 # from thrid parties
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 # my own creations
 from .models import Post # the . in from of model is to let it know to search in current folder for file named models
+from .forms import PostForm
 
 # Create your views here.
 
@@ -18,3 +19,36 @@ def post_list(request):  # passing request object - could have loads of informat
     time_now = datetime.now(timezone(timedelta(hours=-5 )))
     posts = Post.objects.filter(published_date__lte=time_now).order_by('published_date') # render post_list.html -- empty list {} shows we are not passing into any variables  
     return render(request, 'blog/post_list.html', {'posts': posts}) 
+
+def post_detail(request, pk): # web requests for requesst - pk is the url that is defined in url.py file int:pk 
+    post = get_object_or_404(Post, pk=pk) # creating post object to get the object or display 404
+    return render(request, 'blog/post_detail.html', {'post': post}) # render page and pass post:post into the view as post variable
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid(): # if form is valid, save it submit author and date
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = datetime.now() # change timestamp to datetime.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk) # redirect does get operations on teh post detail page where Pk = post.pk
+    else:
+        form = PostForm()# class importing from forms.py
+        
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk) # get post and existing key for post
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = datetime.now() # change timestamp to datetime.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})

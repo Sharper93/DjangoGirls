ffc5df81018663788f569fe253a0ad6102451e34
Sammi Harper <scharper93@gmail.com>
2025-02-26 21:05:08 -0500

    Adding security.login/logout
    Logout shows 405 error

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from datetime import datetime, timezone, timedelta
 
 # from thrid parties
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # my own creations
 from .models import Post # the . in from of model is to let it know to search in current folder for file named models
@@ -25,13 +27,14 @@ def post_detail(request, pk): # web requests for requesst - pk is the url that i
     return render(request, 'blog/post_detail.html', {'post': post}) # render page and pass post:post into the view as post variable
 
 
+@login_required # adding decorator for login_required -included with Django
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid(): # if form is valid, save it submit author and date
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = datetime.now() # change timestamp to datetime.now()
+            post.published_date = datetime.now() # change timezone to datetime.now()
             post.save()
             return redirect('post_detail', pk=post.pk) # redirect does get operations on teh post detail page where Pk = post.pk
     else:
@@ -39,6 +42,7 @@ def post_new(request):
         
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required # adding decorator for login_required -included with Django
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk) # get post and existing key for post
     if request.method == "POST":
@@ -46,9 +50,12 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = datetime.now() # change timestamp to datetime.now()
+            post.published_date = datetime.now() # change timezone to datetime.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import logout
+
+from django.contrib.auth import views # adding views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
     path('', include('blog.urls')), #'' empty string AKA root of system -- including blogs url from blog folder to root system. 
 ]
